4b31e9b2167ba7a742ac4d22881b5b8a3e8f39b8
michalpoznanski <michal.p.poznanski@gmail.com>
2025-08-02 23:23:13 +0200

    Fix: Ustawienie portu 8080 na sztywno dla Railway

--- main.py
+++ main.py
@@ -102,7 +102,4 @@ async def get_stats():
 
 if __name__ == "__main__":
     import uvicorn
-    import os
-
-    port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("main:app", host="0.0.0.0", port=port) 
+    uvicorn.run(app, host="0.0.0.0", port=8080) 
